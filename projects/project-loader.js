f6911207c6cfcf9c3b857437dd29a8200cd89c87
Nathan Savard <savardnm@gmail.com>
2020-09-09 22:36:54 -0400

    create thumbnail through javascript

--- projects/project-loader.js
+++ projects/project-loader.js
@@ -1,5 +1,66 @@
 console.log("loading projects");
-var json = $.getJSON("test.json");
-console.log(json);
+
+let projects = ["test-project"];
+const gridSize = 300;
+
+var container = document.getElementById("container")
+var rows = [document.createElement("div")];
+
+
+for (var i =0; i < projects.length; i++){
+    console.log(projects[i]);
+
+    let columns = Math.round(screen.width / gridSize);
+
+    if(i = columns){
+
+    }
+
+    var it = createProjecTile("test-project");
+    container.append(it)
+
+    console.log("succ");
+
+}
+
+
+function createProjecTile(name) {
+
+    var thumbnail = document.createElement("div");
+    thumbnail.className = "thumbnail";
+
+    var link = document.createElement("a")
+    link.href = "index.html";
+    thumbnail.append(link);
+
+    var tint = document.createElement("div");
+    tint.className = "tint";
+    link.append(tint);
+
+    var banner = document.createElement("div");
+    tint.className = "banner";
+    link.append(banner);
+
+    var image = document.createElement("img")
+    image.src = "projects/" + name + "/cover-image.png";
+    link.append(image);
+    console.log("projects/" + name + "/cover-image.png");
+
+    return thumbnail;
+
+}
+
+/*
+<div class="thumbnail" href="index.html", style="background-image: url(projects/test-project/cover-image.png);">
+    <a href="index.html">
+
+        <div class=tint></div>
+        <div class=banner></div>
+        <img src="projects/test-project/cover-image">
+    </a>
+</div>
+
+ */
+
 
 
