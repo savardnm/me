1e89afd7ece8115bdc8fc3005e4bf7833772ee11
Nathan Savard <savardnm@gmail.com>
2020-09-10 01:17:07 -0400

    upload real projects and cover-images

--- projects/project-database.js
+++ projects/project-database.js
@@ -1,7 +1,39 @@
 var projects = [
     one = {
-        name: "test-project",
-        desc: "a project to test the system",
-        date: new Date('1999-10-06T03:24:00')
+        folder: "RBE-1001",
+        name: "Introduction to Robotics",
+        desc: "An introduction to basic concepts of robotics from ME, CS, and ECE",
+        date: new Date('2019-04-30T03:24:00'),
+        keywords: "Intro,RBE,CS,ME,ECE",
+    },
+    two = {
+        folder: "RBE-2001",
+        name: "Unified Robotics I: Actuation",
+        desc: "A course focused on modeling, analyzing, and fabricating robotic actuation",
+        date: new Date('2019-10-11T03:24:00'),
+        keywords: "RBE,CS,ME,ECE,force,analysis,solidworks,solid-works,duck,PID"
+    },
+    three = {
+        folder: "RBE-2002",
+        name: "Unified Robotics II: Sensing",
+        desc: "A course focused on precision input and output for robotic navigation and motion",
+        date: new Date('2019-12-16T03:24:00'),
+        keywords: "RBE,CS,ME,ECE,sensing,PID,filter,kinematics,band-pass,electrical,analysis"
+    },
+    four = {
+        folder: "study@WPI",
+        name: "Study @ WPI",
+        desc: "A hackathon project designed to help students find study space on campus",
+        date: new Date('2020-01-19T03:24:00'),
+        keywords: "CS,HTML,HTML5,javascript,css,website,github,hackathon,study,WPI"
+    },
+    five = {
+        folder: "CS3733",
+        name: "Software Engineering",
+        desc: "A term project following an Agile Scrum development cycle to create a final product showcased for sponsors",
+        date: new Date('2020-02-22T03:24:00'),
+        keywords: "CS,Java,Jpheonix,FXML,design,software,UI,UX,agile,scrum,team,Navigation,algorithm,database,SQL,master"
     }
 ]
+
+
--- projects/project-loader.js
+++ projects/project-loader.js
@@ -2,14 +2,14 @@ console.log("loading projects");
 
 const tileSize = 300;
 
-var container = document.getElementById("container")
-var rows = [document.createElement("div")];
+var container = document.getElementById("container");
+var attempted = 0;
 
 projects.forEach(function (project, index) {
-    container.append(createProjecTile(project.name))
+    container.append(createProjecTile(project.folder, project.name))
 });
 
-function createProjecTile(name) {
+function createProjecTile(folder, name) {
 
     var tile = document.createElement("div");
     tile.className = "tile";
@@ -24,13 +24,26 @@ function createProjecTile(name) {
 
     var banner = document.createElement("div");
     banner.className = "banner";
+    banner.textContent = name;
     link.append(banner);
 
+    let path = "projects/" + folder + "/cover-image";
+
     var thumbnail = document.createElement("img");
-    thumbnail.src = "projects/" + name + "/cover-image.png";
+    thumbnail.src = path + ".png";
+    thumbnail.onerror = function () { // when .png failed
+        if(attempted > 5){  //if no cover image exists
+            thumbnail.src = "projects/WPI_logo.png";
+            attempted = 0;
+            return;
+        }
+        thumbnail.src = path + '.jpg';
+        attempted ++;
+    };
+
     thumbnail.className = "thumbnail";
     link.append(thumbnail);
-    console.log("projects/" + name + "/cover-image.png");
+    console.log("projects/" + folder + "/cover-image.png");
 
     if (thumbnail.width > thumbnail.height) {
         thumbnail.height = tileSize;
